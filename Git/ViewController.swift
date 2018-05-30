3479eaca86b816f43435eb8e7ee6a97b611354b0
brn.developers <brn.developers@achiever-117.local>
2018-05-30 17:49:50 +0530

    added one line

--- Git/ViewController.swift
+++ Git/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("hiii alivelu")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
